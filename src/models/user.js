2626969845d9ac8a42caf58165ef1231ac9729f8
eddie <edwardobande36@gmail.com>
2019-11-24 14:52:50 +0100

    remove unique email constraint

--- src/models/user.js
+++ src/models/user.js
@@ -33,7 +33,7 @@ const userSchema = new mongoose.Schema({
 );
 
 
-userSchema.index({ email: 1}, { unique: true});
+//userSchema.index({ email: 1}, { unique: true});
 
 userSchema.statics.findByLogin = async function(username) {
   let user = await this.findOne({
